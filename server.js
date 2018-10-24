4fe1f801796f623e2ccce52da0278ca2601941e1
SamualYen <35550675+SamualYen@users.noreply.github.com>
2018-10-24 13:58:02 +0900

    main

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var ws = require("nodejs-websocket");
 console.log("Begin to build websocket...")
 
 const PORT = process.env.PORT || 8080;
-const PORT2 = process.env.PORT || 8081;
 
 var screenReady = false;
 var pepperReady = false;
@@ -37,6 +36,28 @@ var server = ws.createServer(function(conn) {
             conn.sendText("Pepper Pong");
             console.log("Sended data is:Pepper Pong")
         }
+        
+        if (str === "mscreen") {
+            mscreen = conn;
+            mscreenReady = true;
+            console.log("miyako Screen is ready")
+        }
+        if (str === "mpepper") {
+            mpepper = conn;
+            mpepperReady = true;
+            console.log("miyako Pepper is ready")
+        }
+        if (mscreenReady && mpepperReady) {
+            mscreen.sendText(str);
+        }
+        if (str === "miyako Screen Ping") {
+            conn.sendText("miyako Screen Pong");
+            console.log("Sended data is:miyako Screen Pong")
+        }
+        if (str === "miyako Pepper Ping") {
+            conn.sendText("miyako Pepper Pong");
+            console.log("Sended data is:miyako Pepper Pong")
+        }
         conn.sendText(str);
     })
     conn.on("close", function(code, reason) {
@@ -48,37 +69,5 @@ var server = ws.createServer(function(conn) {
 }).listen(PORT)
 console.log("WebSocket has done.")
 
-var server2 = ws.createServer(function(mconn) {
-    mconn.on("text", function(str) {
-        console.log("Received data is:" + str)
-        if (str === "mscreen") {
-            mscreen = mconn;
-            mscreenReady = true;
-            console.log("miyako Screen is ready")
-        }
-        if (str === "mpepper") {
-            mpepper = mconn;
-            mpepperReady = true;
-            console.log("miyako Pepper is ready")
-        }
-        if (mscreenReady && mpepperReady) {
-            mscreen.sendText(str);
-        }
-        if (str === "miyako Screen Ping") {
-            mconn.sendText("miyako Screen Pong");
-            console.log("Sended data is:miyako Screen Pong")
-        }
-        if (str === "miyako Pepper Ping") {
-            mconn.sendText("miyako Pepper Pong");
-            console.log("Sended data is:miyako Pepper Pong")
-        }
-        mconn.sendText(str);
-    })
-    mconn.on("close", function(code, reason) {
-        console.log("Miyako Websocket closed")
-    });
-    mconn.on("error", function(code, reason) {
-        console.log("Miyako Websocket error")
-    });
-}).listen(PORT2)
-console.log("Miyako WebSocket has done.")
+
+
